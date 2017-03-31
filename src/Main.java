f9ab04fdb2dc9832adafd418f3dc1cf1bc375dac
sethu <sethu.store@gmail.com>
2017-03-31 11:44:16 +0530

    Toggle String

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        
+        Scanner scanner= new Scanner(System.in);
+        String line= scanner.nextLine();
+        char[] charArr= line.toCharArray();
+
+
+        for(int c:charArr){
+            int ch=c;
+            if(c>=65 && c<=90){
+                ch=c+32;
+            }else if(c>=97 && c<=122){
+                ch=c-32;
+            }
+            System.out.print((char)ch);
+        }
+        scanner.close();
+
     }
 }
